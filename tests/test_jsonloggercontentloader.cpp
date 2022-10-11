17cab0bac4bec85463a5728d298db800d010b457
Andreas Müller <schnitzeltony@gmail.com>
2022-10-11 20:03:01 +0200

    Add test_jsonloggercontentloader::compareSessionLogConfigFileBasenamesEqual
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/test_jsonloggercontentloader.cpp
+++ tests/test_jsonloggercontentloader.cpp
@@ -1,6 +1,7 @@
 #include "test_jsonloggercontentloader.h"
 #include "jsonloggercontentloader.h"
 #include <QTest>
+#include <QFileInfo>
 
 static const char *validJsonFileName = "contentset_valid.json";
 static const char *emptyJsonFileName = "contentset_empty.json";
@@ -51,6 +52,25 @@ void test_jsonloggercontentloader::compareSessionLogConfigFileCountEqual()
     QStringList sessionFileList = QString(SESSION_FILES).split(",");
     QStringList loggerFileList = QString(LOGGER_CONFIG_FILES).split(",");
     QCOMPARE(sessionFileList.count(), loggerFileList.count());
+}
+
+static void toBaseName(QStringList &fileList)
+{
+    for(auto& file : fileList) {
+        QFileInfo fi(file);
+        file = fi.baseName();
+    }
+}
+
+void test_jsonloggercontentloader::compareSessionLogConfigFileBasenamesEqual()
+{
+    QStringList sessionFileList = QString(SESSION_FILES).split(",");
+    sessionFileList.sort();
+    toBaseName(sessionFileList);
+    QStringList loggerFileList = QString(LOGGER_CONFIG_FILES).split(",");
+    loggerFileList.sort();
+    toBaseName(loggerFileList);
+    QCOMPARE(sessionFileList, loggerFileList);
 }
 
 QTEST_MAIN(test_jsonloggercontentloader)
